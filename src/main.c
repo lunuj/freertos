4663eb97bc04d2ffc1119b4b4ad3baea470fda8c
lunuj <68381451+lunuj@users.noreply.github.com>
2023-11-10 01:16:11 +0800

    ✨ feat(主模块): 创建点灯任务
    通过GPIOC_13每500ms反转电平

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 /*
  * @Author: lunuj
  * @Date: 2023-10-29 23:38:03
- * @LastEditTime: 2023-10-30 00:02:02
+ * @LastEditTime: 2023-11-10 01:10:28
  * @FilePath: \freertos\src\main.c
  * @Description: 
  * 
  * Copyright (c) 2023 by lunuj, All Rights Reserved. 
  */
 #include "main.h"
+TaskHandle_t Task_Handler;
+void led_task(void *pvParameters);
+
 
 int main(void)
 {
@@ -16,11 +19,28 @@ int main(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_SetBits(GPIOC, GPIO_Pin_13);
+
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
-//	GPIO_SetBits(GPIOC, GPIO_Pin_13);
-	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+
+	OLED_Init();
+	OLED_ShowString(1,1,"Running");
+
+	xTaskCreate(
+    (TaskFunction_t)	led_task,
+    (const char *)		"led_task",
+    (uint16_t)			128,
+    (void *)			NULL,
+    (UBaseType_t)		3,
+    (TaskHandle_t)		&Task_Handler);
+
+	vTaskStartScheduler();
+}
+
+void led_task(void *pvParameters){
 	while (1)
 	{
-		
+		GPIO_WriteBit(GPIOC,GPIO_Pin_13,(BitAction)!GPIO_ReadOutputDataBit(GPIOC,GPIO_Pin_13));
+		FreeRTOS_delay_ms(500);
 	}
 }
--- src/main.h
+++ src/main.h
@@ -1,7 +1,14 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#include "./../.eide/deps/start/stm32f10x.h"                  // Device header
+#include "stm32f10x.h"
+#include "Delay.h"
+#include "OLED.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "My_FreeRTOS.h"
+
 
 
 #endif // __MAIN_H__
+
