1d27dd576bf8df86eb9f662327949841fc6b1af8
lunuj <68381451+lunuj@users.noreply.github.com>
2023-11-13 01:42:08 +0800

    🐞 fix(按键模块): 修改触发按键导致系统阻塞
    将按键检测函数中系统延时替换为任务延时即可解决

--- lib/Key.h
+++ lib/Key.h
@@ -1,8 +1,8 @@
 /*
  * @Author: lunuj
  * @Date: 2023-09-18 15:46:45
- * @LastEditTime: 2023-09-20 01:05:42
- * @FilePath: \test\Hardware\Key.h
+ * @LastEditTime: 2023-11-13 00:10:17
+ * @FilePath: \freertos\lib\Key.h
  * @Description: led头文件
  * 
  * Copyright (c) 2023 by lunuj, All Rights Reserved. 
@@ -11,7 +11,9 @@
 #ifndef __KEY_H
 #define __KEY_H
 
-#define Key_0 GPIO_Pin_5
+#define Key_Port GPIOC
+#define Key_Periph RCC_APB2Periph_GPIOA
+#define Key_0 GPIO_Pin_14
 #define Key_1 GPIO_Pin_6
 
 void Key_Init(void);
